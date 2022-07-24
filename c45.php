b8bcd6671a4483beb3098f8551fdfbc06e1d7b3a
RozliyanaLatola <rozliyanalatola@gmail.com>
2022-07-24 17:02:57 +0700

    committ

--- c45.php
+++ c45.php
@@ -1,7 +1,7 @@
 <?php
 $mp1 = [
-    'Sepeda', //0
-    'Jalan Kaki', //1
+    'Jalan Kaki', //0
+    'Sepeda', //1
     'Sepeda Motor'
 ]; //2
 $mp2 = [
@@ -26,9 +26,9 @@ $mp5 = [
 ]; //2
 $mp6 = [
     'Kurang dari Rp. 500,000', //0
-    'Rp. 500,000 - Rp. 999,999', //1
-    'Rp. 1,000,000 - Rp. 1,999,999', //2
-    'Rp. 2,000,000 - Rp. 4,999,999', //3
+    'Rp. 1,000,000 - Rp. 1,999,999', //1
+    'Rp. 2,000,000 - Rp. 4,999,999', //2
+    'Rp. 500,000 - Rp. 999,999', //3
     'Tidak Berpenghasilan'
 ]; //4
 
@@ -39,24 +39,24 @@ $data = [];
 //1
 $d = new stdClass();
 $d->nama = 'ADITYA PUTRA ALMANDA';
-$d->p1 = $mp1[0];
+$d->p1 = $mp1[1];
 $d->p2 = $mp2[2];
 $d->p3 = $mp3[1];
 $d->p4 = $mp4[2];
 $d->p5 = $mp5[2];
-$d->p6 = $mp6[2];
+$d->p6 = $mp6[1];
 $d->p7 = $mp7[1];
 $data[] = $d;
 
 //2
 $d = new stdClass();
 $d->nama = 'ADZIN MUHAMMAD ABIYYU';
-$d->p1 = $mp1[1];
+$d->p1 = $mp1[0];
 $d->p2 = $mp2[0];
 $d->p3 = $mp3[0];
 $d->p4 = $mp4[0];
 $d->p5 = $mp5[2];
-$d->p6 = $mp6[1];
+$d->p6 = $mp6[3];
 $d->p7 = $mp7[0];
 $data[] = $d;
 
@@ -68,26 +68,26 @@ $d->p2 = $mp2[0];
 $d->p3 = $mp3[0];
 $d->p4 = $mp4[0];
 $d->p5 = $mp5[1];
-$d->p6 = $mp6[3];
+$d->p6 = $mp6[2];
 $d->p7 = $mp7[1];
 $data[] = $d;
 
 //4
 $d = new stdClass();
 $d->nama = 'PARNO RANDI';
-$d->p1 = $mp1[1];
+$d->p1 = $mp1[0];
 $d->p2 = $mp2[0];
 $d->p3 = $mp3[0];
 $d->p4 = $mp4[0];
 $d->p5 = $mp5[0];
-$d->p6 = $mp6[1];
+$d->p6 = $mp6[3];
 $d->p7 = $mp7[1];
 $data[] = $d;
 
 //5
 $d = new stdClass();
 $d->nama = 'REFALDA RAMZI';
-$d->p1 = $mp1[1];
+$d->p1 = $mp1[0];
 $d->p2 = $mp2[2];
 $d->p3 = $mp3[2];
 $d->p4 = $mp4[2];
@@ -99,7 +99,7 @@ $data[] = $d;
 //6
 $d = new stdClass();
 $d->nama = 'PUTRI AMELLIZA';
-$d->p1 = $mp1[1];
+$d->p1 = $mp1[0];
 $d->p2 = $mp2[0];
 $d->p3 = $mp3[0];
 $d->p4 = $mp4[1];
@@ -116,7 +116,7 @@ $d->p2 = $mp2[2];
 $d->p3 = $mp3[1];
 $d->p4 = $mp4[2];
 $d->p5 = $mp5[1];
-$d->p6 = $mp6[1];
+$d->p6 = $mp6[3];
 $d->p7 = $mp7[0];
 $data[] = $d;
 
@@ -128,14 +128,14 @@ $d->p2 = $mp2[0];
 $d->p3 = $mp3[0];
 $d->p4 = $mp4[1];
 $d->p5 = $mp5[2];
-$d->p6 = $mp6[1];
+$d->p6 = $mp6[3];
 $d->p7 = $mp7[1];
 $data[] = $d;
 
 //9
 $d = new stdClass();
 $d->nama = 'MELSI AGUSNI';
-$d->p1 = $mp1[0];
+$d->p1 = $mp1[1];
 $d->p2 = $mp2[0];
 $d->p3 = $mp3[0];
 $d->p4 = $mp4[0];
@@ -159,7 +159,7 @@ $data[] = $d;
 //11
 $d = new stdClass();
 $d->nama = 'REGI ASIRGA DARLIS';
-$d->p1 = $mp1[1];
+$d->p1 = $mp1[0];
 $d->p2 = $mp2[1];
 $d->p3 = $mp3[0];
 $d->p4 = $mp4[1];
@@ -171,12 +171,12 @@ $data[] = $d;
 //12
 $d = new stdClass();
 $d->nama = 'REGO FITRA AGUS';
-$d->p1 = $mp1[1];
+$d->p1 = $mp1[0];
 $d->p2 = $mp2[1];
 $d->p3 = $mp3[0];
 $d->p4 = $mp4[1];
 $d->p5 = $mp5[0];
-$d->p6 = $mp6[3];
+$d->p6 = $mp6[2];
 $d->p7 = $mp7[0];
 $data[] = $d;
 
@@ -188,7 +188,7 @@ $d->p2 = $mp2[1];
 $d->p3 = $mp3[0];
 $d->p4 = $mp4[0];
 $d->p5 = $mp5[0];
-$d->p6 = $mp6[2];
+$d->p6 = $mp6[1];
 $d->p7 = $mp7[1];
 $data[] = $d;
 
